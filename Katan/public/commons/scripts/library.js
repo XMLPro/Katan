864eebd6e6ac02c693ed0384e1c126062ef78a83
qzpm <tarako2000yen@yahoo.co.jp>
2016-07-01 12:14:10 +0900

    タイルの数字作成機能の実装

--- Katan/public/commons/scripts/library.js
+++ Katan/public/commons/scripts/library.js
@@ -1,5 +1,5 @@
-// マップ描画ライブラリ
-class paintMap {
+// マップ描画
+class PaintMap {
   constructor(ctx) {
     this.ctx = ctx;
   }
@@ -17,11 +17,40 @@ class paintMap {
 
   // 六角形の全体配置を決定する関数
   paintHexagons(row = 5, tileColor) {
+    let count = 0
     for(let i = 1; i <= row; i++) {
       for(let j = 1; j <= row - Math.abs((row - 2) - i); j++) {
         // 座標のずらし方は適当
         this.paintHexagon(j * 92 + 45 * Math.abs((row - 2) - i) - 20, i * 80, 50, tileColor)
+        count++
       }
     }
+    return count
+  }
+
+  paintNum(tiles) {
+    // 未実装
+    console.log(tiles[0].getNumber())
   }
 }
+
+class Tile {
+  constructor(resource, number) {
+    this.resource = resource
+    this.number = number
+  }
+  getResource() {
+    return this.resource
+  }
+  getNumber() {
+    return this.number
+  }
+}
+
+const GenerateTile = ((tileNum) => {
+  const tiles = new Array(tileNum)
+  for(let i = 0; i < tileNum; i++) {
+    tiles[i] = new Tile(0, 6, i)
+  }
+  return tiles
+})
