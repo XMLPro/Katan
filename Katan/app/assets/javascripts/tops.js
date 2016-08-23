d7abfd427971f1bbd783f1143d837c6431f44c12
Keita <k.keita811.anuy@gmail.com>
2016-08-23 00:58:17 -0700

    add coordinate

--- Katan/app/assets/javascripts/tops.js
+++ Katan/app/assets/javascripts/tops.js
@@ -9,11 +9,14 @@ $(() => {
   const pm = new PaintMap(ctx)          // ライブラリを利用
 
   const tiles = hoge.init()
-
   // ゲームループ とりあえず60FPSぐらいで描画
   setInterval(() => {
     pm.paintHexagons(5, tiles)            // タイルの描画
     pm.paintNum(tiles)                    // 数字の描画
+    ctx.fillStyle = '#FF3333'
+    for(var i=0; i<6; i++){
+      ctx.fillRect(tiles[0].coordinates[i][0]-10, tiles[0].coordinates[i][1]-10, 20, 20)
+    }
   }, 1000 / 60)
 
    var ws = new WebSocketRails("localhost:3000/websocket");
@@ -25,4 +28,5 @@ $(() => {
     ws.bind("chat_receive", function (data) {
       $("#msg_view").prepend("<li>" + data + "</li>")
     })
+    
 })
