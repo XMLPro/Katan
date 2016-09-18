e5aaf4f3f4968c0adc1a9c48f48962bba22177eb
CsTarepanda <sankaku900622@yahoo.co.jp>
2016-09-18 20:45:14 +0900

    交差点にくっついてた余分なクラスを削除

--- Katan/app/assets/javascripts/tops.js
+++ Katan/app/assets/javascripts/tops.js
@@ -34,9 +34,10 @@ $(() => {
         let x = map_coordinate["left"];
         let y = map_coordinate["top"];
         for(let i = 0; i < intersections.length; i++){
-            $(intersections[i])
-                .css("left", x + rects[i][0] - intersections[i].width / 2 + "px")
-                .css("top", y + rects[i][1] - intersections[i].height / 2 + "px");
+            $(intersections[i]).css({
+                left: x + rects[i][0] - intersections[i].width / 2 + "px",
+                top: y + rects[i][1] - intersections[i].height / 2 + "px"
+            })
         }
         // ゲームループ とりあえず60FPSぐらいで描画
         pm.paintHexagons(5, tiles)            // タイルの描画
