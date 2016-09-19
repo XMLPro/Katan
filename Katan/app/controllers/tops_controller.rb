dba54a788312cf3337379ccef1f270790739e8be
CsTarepanda <sankaku900622@yahoo.co.jp>
2016-09-19 14:16:09 +0900

    再読み込み時に建物の描画がされるように変更

--- Katan/app/controllers/tops_controller.rb
+++ Katan/app/controllers/tops_controller.rb
@@ -1,6 +1,6 @@
 class TopsController < ApplicationController
   def index
-    map_clear
+    # map_clear
     @map = map_init
     @fields = @map.game_fields
     @intersections = @map.game_intersections
