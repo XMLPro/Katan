5b7decf39b919e6afa6fc239060a02ead109db78
CsTarepanda <sankaku900622@yahoo.co.jp>
2016-09-19 15:19:47 +0900

    add turn to map

--- Katan/app/models/game_map.rb
+++ Katan/app/models/game_map.rb
@@ -1,3 +1,14 @@
 class GameMap < ActiveRecord::Base
   belongs_to :game_field
+  has_many :turns
+
+  def next_turn
+    t_number = self.turn_number + 1
+    t_number = 0 if self.max_member <= t_number
+    self.update turn_number: t_number
+  end
+
+  def turn_user
+    self.turns.find_by_number(self.turn_number)
+  end
 end
--- Katan/db/migrate/20160818054948_create_game_maps.rb
+++ Katan/db/migrate/20160818054948_create_game_maps.rb
@@ -2,6 +2,8 @@ class CreateGameMaps < ActiveRecord::Migration
   def change
     create_table :game_maps do |t|
       t.integer :game_field_id
+      t.integer :max_member, default: 4
+      t.integer :turn_number, default: 0
 
       t.timestamps null: false
     end
