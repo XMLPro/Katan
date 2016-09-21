d033c734a226cf02e62dd380c09461450b91ed61
9shikixp <c0115334ef@edu.teu.ac.jp>
2016-09-21 17:50:19 +0900

    ユーザーのnameからvalidataで\Wを弾いた

--- Katan/app/models/user.rb
+++ Katan/app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   validates :name,  presence: true,
     length: { maximum: 12 },
     uniqueness: true
+  validates :name, format: {without: /\W/}
   has_many :game_buildings
   has_many :game_resources
 end
