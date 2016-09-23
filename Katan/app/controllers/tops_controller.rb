3049d62a29dc0559fc2a0bcf96aebe5f6e771383
9shikixp <c0115334ef@edu.teu.ac.jp>
2016-09-23 14:30:57 +0900

    複数個のmapが引っかかるようになった

--- Katan/app/controllers/tops_controller.rb
+++ Katan/app/controllers/tops_controller.rb
@@ -12,13 +12,14 @@ class TopsController < ApplicationController
 
   def get_resources
   	@dice = dice
-  	@get_maps = GameField.find_by(number:@dice)
-
- 		for v in @get_maps.vertices.all do
- 			if v.game_intersection.game_building
- 				@resource_type_id = @get_maps.resource_type_id
- 				GameResource.create(user: current_user, resource_type: ResourceType.find_by_id(@resource_type_id))
- 			end
-  	end
+  	@get_maps = GameField.where(number:@dice)
+    for m in @get_maps do
+      for v in m.vertices.all do
+ 			  if v.game_intersection.game_building
+ 				 @resource_type_id = m.resource_type_id
+ 				 GameResource.create(user: current_user, resource_type: ResourceType.find_by_id(@resource_type_id))
+ 			  end
+  	 end
+    end
   end
 end
