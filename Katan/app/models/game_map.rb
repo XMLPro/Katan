b93b821ebaba59ccb5c3169d21d0344dc1b2ab45
CsTarepanda <sankaku900622@yahoo.co.jp>
2016-09-25 22:06:05 +0900

    初期ターン実装 column追加/first:boolean first2:boolean

--- Katan/app/models/game_map.rb
+++ Katan/app/models/game_map.rb
@@ -5,9 +5,33 @@ class GameMap < ActiveRecord::Base
   has_many :game_fields
 
   def next_turn
-    t_number = self.turn_number + 1
-    t_number = 0 if self.max_member <= t_number
-    self.update turn_number: t_number
+    unless first?
+      t_number = self.turn_number + 1
+      t_number = 0 if self.max_member <= t_number
+      self.update turn_number: t_number
+    end
+  end
+
+  def next_first_turn
+    if first?
+      if self.first
+        t_number = self.turn_number + 1
+        if self.max_member <= t_number
+          self.first = false
+          t_number = self.turn_number
+        end
+      else
+        t_number = self.turn_number - 1
+        if t_number <= 0
+          self.first2 = false
+        end
+      end
+      self.update turn_number: t_number, first: self.first, first2: self.first2
+    end
+  end
+
+  def first?
+    self.first || self.first2
   end
 
   def current_turn
--- Katan/db/migrate/20160818054948_create_game_maps.rb
+++ Katan/db/migrate/20160818054948_create_game_maps.rb
@@ -3,6 +3,8 @@ class CreateGameMaps < ActiveRecord::Migration
     create_table :game_maps do |t|
       t.integer :max_member, default: 4
       t.integer :turn_number, default: 0
+      t.boolean :first, default: true
+      t.boolean :first2, default: true
       t.timestamps null: false
     end
   end
