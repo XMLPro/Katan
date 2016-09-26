10630c72522fa83ef432bd1bec52563e14037eb0
CsTarepanda <sankaku900622@yahoo.co.jp>
2016-09-26 23:38:33 +0900

    draw info data

--- Katan/app/models/user.rb
+++ Katan/app/models/user.rb
@@ -6,4 +6,8 @@ class User < ActiveRecord::Base
   has_many :game_buildings
   has_many :game_resources
   has_one :turn
+
+  def resources(name)
+    self.game_resources.where(resource_type: ResourceType.find_by(name: name))
+  end
 end
