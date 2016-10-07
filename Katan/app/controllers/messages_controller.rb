1810e4d7bfa0a6fc0fa5cc4327b0a818d6dd8a11
9shikixp <c0115334ef@edu.teu.ac.jp>
2016-10-07 12:38:30 +0900

    escape html in chat

--- Katan/app/controllers/messages_controller.rb
+++ Katan/app/controllers/messages_controller.rb
@@ -1,6 +1,6 @@
 class MessagesController < WebsocketRails::BaseController
   include ActionController
   def new
-    broadcast_message :chat_receive, '<span style = "color:blue">' + current_user.name + '</span> : ' + message
+    broadcast_message :chat_receive, '<span style = "color:blue">' + current_user.name + '</span> : ' + ERB::Util.html_escape(message)
   end
 end
