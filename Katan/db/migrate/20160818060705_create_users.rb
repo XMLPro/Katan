361be3ef54f72f75dd2cdd5b830bdb7d81e74459
CsTarepanda <sankaku900622@yahoo.co.jp>
2016-10-21 09:44:18 +0900

    クッキーが残ってて、ログインしていないユーザーに入れる問題修正 * userにuser_secret_token追加 * クッキー使ってログインする時に、クッキーのsecret_tokenと、userのuser_secret_tokenを比較する

--- Katan/db/migrate/20160818060705_create_users.rb
+++ Katan/db/migrate/20160818060705_create_users.rb
@@ -3,6 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :name
       t.integer :point
+      t.string :secret_token
 
       t.timestamps null: false
     end
